01549e421fed8f5f1fab4e6291ad5746e34ba079
ErickTech <erickestradatech@gmail.com>
2021-04-08 16:33:56 -0500

    completed project

---
+++ src/main/java/clases/Personal.java
@@ -0,0 +1,39 @@
+package clases;
+
+public class Personal {
+
+    private int codigo;
+    private String nombre;
+    private int anioIngreso;
+
+    public Personal(int codigo, String nombre, int anioIngreso) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.anioIngreso = anioIngreso;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioIngreso() {
+        return anioIngreso;
+    }
+
+    public void setAnioIngreso(int anioIngreso) {
+        this.anioIngreso = anioIngreso;
+    }
+
+}
